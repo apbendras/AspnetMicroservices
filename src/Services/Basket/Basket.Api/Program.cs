f6d771cbb28befe58132ae256929d5302344ee21
apbendras <apbendras@gmail.com>
2022-10-04 22:57:51 +0300

    Basket repo added

--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(options => {
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
 
 var app = builder.Build();
 
---
+++ src/Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.Api.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _redisCache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (basket != null)
+            {
+                await _redisCache.RemoveAsync(userName);
+            }
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonSerializer.Deserialize<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
+
